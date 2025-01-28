b43215b7a6c69b1ba3e7d28e0453cd215a235143
Vítor Zermiani Leopoldo <vitor.leopoldo@selbetti.local>
2025-01-28 18:27:14 -0300

    Adicionando Schema ao retorno do endpoint raiz

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from app import app
 
 
-def test_reasd_root_dedve_retornar_OK_ola_mundo():
+def test_read_root_dedve_retornar_OK_ola_mundo():
     client = TestClient(app)
 
     response = client.get("/")
